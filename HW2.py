cfe9482bc5d6e0464fcbc16425d521a1f3f14445
hiba zalloum <hebahzalloum@gmail.com>
2020-10-30 01:17:10 +0300

    sixth commit

--- HW2.py
+++ HW2.py
@@ -1,19 +1,5 @@
 movies = open('movies.txt', encoding="utf-8")
 
-termDict = {}
-kgramDict= {}
-
-with open('movies.txt', 'r') as movie:
-    for line in movie:
-        words = line.replace('.', '').replace('\'', '').replace(',', '').lower().split()
-        for word in words:
-            g = K_Gram (3, trem)
-            if word not in termDict:
-                termDict[word] = 1
-            else:
-                termDict[word] = termDict[word] + 1
-
-
 def k_Gram(k, text):
     klist = []
     for i in range(len(text) - (k - 1) ):
@@ -39,4 +25,21 @@ def edit_distance(term, query, x, y):
                 distProb[n][m]= 1 + min(distProb[n][m-1],distProb[n-1][m],distProb[n-1][m-1])
     return distProb[x][y]
 
-def search ()
+def search ():
+    pass
+
+termDict = {}
+kgramDict= {}
+
+with open('movies.txt', 'r') as movie:
+    for line in movie:
+        words = line.replace('.', '').replace('\'', '').replace(',', '').lower().split()
+        for word in words:
+            g =k_Gram(3, word)
+            for k_Gram in g:
+                if k_Gram in kgramDict:
+                    kgramDict[k_Gram].append(word)
+                else:
+                    kgramDict[k_Gram] = [word]
+            if word not in termDict.keys():
+                termDict[word]= g
