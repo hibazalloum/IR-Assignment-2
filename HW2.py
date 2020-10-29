e6e4f5ae12da232f51e8c44de5fa11f014ae029d
hiba zalloum <hebahzalloum@gmail.com>
2020-10-30 02:10:51 +0300

    seventh commit

--- HW2.py
+++ HW2.py
@@ -1,5 +1,6 @@
 movies = open('movies.txt', encoding="utf-8")
 
+
 def k_Gram(k, text):
     klist = []
     for i in range(len(text) - (k - 1) ):
@@ -25,21 +26,50 @@ def edit_distance(term, query, x, y):
                 distProb[n][m]= 1 + min(distProb[n][m-1],distProb[n-1][m],distProb[n-1][m-1])
     return distProb[x][y]
 
-def search ():
-    pass
+jaccard = 0.1
+jc =0
+factor = 2
+wd = {}
+highjaccrd =[]
+
+
+def search(q):
+    sg = k_Gram(3,q)
+    for k in sg:
+        highjaccrd.append(kgramDict[k])
+    for i in range(len(highjaccrd)):
+        for j in highjaccrd[i]:
+            high = termDict[j]
+            jaccard =jaccardCoefficient(high,sg)
+            if jaccard > jc:
+                if j not in wd.keys():
+                    wd[j] = jaccard
+    print(wd)
+    highJaccard = list(wd)
+    final = []
+    for word in highJaccard:
+        distence = edit_distance(word,q, len(word),len(q))
+        if distence < factor:
+            final.append(word)
+    print(final)
+
 
 termDict = {}
-kgramDict= {}
+kgramDict = {}
 
 with open('movies.txt', 'r') as movie:
     for line in movie:
         words = line.replace('.', '').replace('\'', '').replace(',', '').lower().split()
         for word in words:
-            g =k_Gram(3, word)
+            g = k_Gram(3, word)
             for k_Gram in g:
                 if k_Gram in kgramDict:
                     kgramDict[k_Gram].append(word)
                 else:
                     kgramDict[k_Gram] = [word]
             if word not in termDict.keys():
-                termDict[word]= g
+                termDict[word] = g
+
+
+query = input("input your query")
+search(query)
